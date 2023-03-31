769ab4f61054464c1c6b24e761f8e8ca8d94f325
Светлана Алексеева <Lanaleks90@gmail.com>
2023-03-31 14:11:15 +0400

    Editing the test

--- src/test/java/ru/netology/card/delivery/test/CardDeliveryTestTask2.java
+++ src/test/java/ru/netology/card/delivery/test/CardDeliveryTestTask2.java
@@ -1,7 +1,7 @@
 package ru.netology.card.delivery.test;
 
 import com.codeborne.selenide.Condition;
-
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,25 +9,28 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
-public class CardDeliveryTest {
-
-
+public class CardDeliveryTestTask2 {
     private String generateDay(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    void shouldTestSuccessfulSending() {
+    void shouldTestInteractionWithComplexElements() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Самара");
-        String date = generateDay(3, "dd.MM.yyyy");
+        $("[data-test-id='city'] input").setValue("Кр");
+        $(byText("Краснодар")).shouldBe(visible).click();
+        String date = generateDay(35, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-step='1']").click();
         $("[data-test-id='date'] input").sendKeys(date);
-        $("[data-test-id='name'] input").setValue("Васильева-Петрова Мария");
+        $("[data-test-id='name'] input").setValue("Чернов Иннокентий");
         $("[data-test-id='phone'] input").setValue("+79384747585");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
