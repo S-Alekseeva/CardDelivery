2ccc0c0cd3d0257e835bbb42c57654b0f2d60616
Светлана Алексеева <Lanaleks90@gmail.com>
2023-04-02 16:32:08 +0400

    Editing the test2

--- src/test/java/ru/netology/card/delivery/test/CardDeliveryTestTask2.java
+++ src/test/java/ru/netology/card/delivery/test/CardDeliveryTestTask2.java
@@ -9,8 +9,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -22,18 +21,26 @@ public class CardDeliveryTestTask2 {
 
     @Test
     void shouldTestInteractionWithComplexElements() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Кр");
         $(byText("Краснодар")).shouldBe(visible).click();
-        String date = generateDay(35, "dd.MM.yyyy");
+        String date = generateDay(3, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-step='1']").click();
-        $("[data-test-id='date'] input").sendKeys(date);
+        int count = $$x("//td[@class='calendar__day']").size();
+        for (int i = 0; i < count; i++) {
+            String text = $$x("//td[@class='calendar__day']").get(i).getText();
+            if (text.equalsIgnoreCase("25")) {
+                $$x("//td[@class='calendar__day']").get(i).click();
+                break;
+            }
+        }
         $("[data-test-id='name'] input").setValue("Чернов Иннокентий");
         $("[data-test-id='phone'] input").setValue("+79384747585");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + date));
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на 25.05.2023"));
     }
 }
